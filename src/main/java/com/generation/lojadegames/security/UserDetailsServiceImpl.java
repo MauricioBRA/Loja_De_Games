1cfe21252a0daeddbc1bdb7481ccaccbeb0bc5d3
Mauricio <mauricio_oliver75@live.com>
2022-05-17 01:21:32 -0300

    Atualização Security

---
+++ src/main/java/com/generation/lojadegames/security/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.generation.lojadegames.security;
+
+import java.nio.charset.Charset;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Optional;
+
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.generation.lojadegames.model.Usuario;
+import com.generation.lojadegames.model.UsuarioLogin;
+import com.generation.lojadegames.repository.UsuarioRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+	
+	@Autowired
+	private UsuarioRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+
+		Optional<Usuario> usuario = userRepository.findByUsuario(userName);
+		usuario.orElseThrow(() -> new UsernameNotFoundException(userName + " not found."));
+
+		return usuario.map(UserDetailsImpl::new).get();
+	}
+}
